ea81bb7c2057bb39a6243460f22e95823c2c0073
Flynn van Os <oxappsdev@gmail.com>
2016-09-25 18:49:36 -0400

    Update copyright notices

--- app/src/main/java/com/oxapps/materialcountdown/CategoryPickerDialog.java
+++ app/src/main/java/com/oxapps/materialcountdown/CategoryPickerDialog.java
@@ -1,21 +1,31 @@
+/*
+ * Copyright 2016 Flynn van Os
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.oxapps.materialcountdown;
 
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialog;
-import android.support.v7.app.AppCompatDialogFragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- app/src/main/java/com/oxapps/materialcountdown/EventDetailActivity.java
+++ app/src/main/java/com/oxapps/materialcountdown/EventDetailActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Flynn van Os
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.oxapps.materialcountdown;
 
 import android.content.Intent;
@@ -19,18 +35,12 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class EventDetailActivity extends AppCompatActivity {
-    @Bind(R.id.tv_event_detail_title)
-    TextView mTitleView;
-    @Bind(R.id.tv_event_detail_desc)
-    TextView mDescView;
-    @Bind(R.id.event_detail_days)
-    TextView mDaysView;
-    @Bind(R.id.event_detail_hours)
-    TextView mHoursView;
-    @Bind(R.id.event_detail_minutes)
-    TextView mMinutesView;
-    @Bind(R.id.event_detail_seconds)
-    TextView mSecondsView;
+    @Bind(R.id.tv_event_detail_title) TextView mTitleView;
+    @Bind(R.id.tv_event_detail_desc) TextView mDescView;
+    @Bind(R.id.event_detail_days) TextView mDaysView;
+    @Bind(R.id.event_detail_hours) TextView mHoursView;
+    @Bind(R.id.event_detail_minutes) TextView mMinutesView;
+    @Bind(R.id.event_detail_seconds) TextView mSecondsView;
 
     private boolean daysEnabled = true;
     private static final String TAG = "EventDetailActivity";
