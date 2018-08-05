27bc29134bc6426a111c3b2e7217155821b4d74c
Flynn van Os <flynnv.os@gmail.com>
2018-08-06 03:56:55 +1200

    Update copyright

--- app/src/main/java/com/oxapps/materialcountdown/detail/EventDetailActivity.kt
+++ app/src/main/java/com/oxapps/materialcountdown/detail/EventDetailActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Flynn van Os
+ * Copyright 2018 Flynn van Os
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
